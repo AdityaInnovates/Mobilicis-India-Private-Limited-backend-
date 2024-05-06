d4694266b6ae805a372ac12a9fa6c71b32208af4
Aditya Kumar <adityakr80510@gmail.com>
2024-05-07 01:48:49 +0530

    UPDATE

--- server.js
+++ server.js
@@ -64,6 +64,8 @@ io.on("connection", (socket) => {
   }
 });
 
-server.listen(5000, () => {
-  console.log("Server is running on http://localhost:5000");
+server.listen(process.env.PORT || 5000, () => {
+  console.log(
+    "Server is running on http://localhost:" + process.env.PORT || 5000
+  );
 });
